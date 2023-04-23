5081bd6e7aab76149c9d3f02973049302d5de306
Risha <knyazeva.irina.94@mail.ru>
2023-04-23 17:52:26 +0300

    Планшетная версия главной страницы и меню поставлено в поток на мобильной версии

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,14 +1,15 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+let mainHeader = document.querySelector('.main-header');
+let headerToggle = document.querySelector('.main-header__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+mainHeader.classList.remove('main-header--nojs');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+headerToggle.addEventListener('click', function () {
+  if (mainHeader.classList.contains('main-header--closed')) {
+    mainHeader.classList.remove('main-header--closed');
+    mainHeader.classList.add('main-header--opened');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    mainHeader.classList.add('main-header--closed');
+    mainHeader.classList.remove('main-header--opened');
   }
 });
+
