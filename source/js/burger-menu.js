2f52154491d4fccc5adb0f9863689d0b61a2ba39
Risha <knyazeva.irina.94@mail.ru>
2023-05-25 23:37:11 +0300

    Изменен фокус радиокнопки

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,8 +1,10 @@
 let mainHeader = document.querySelector('.main-header');
 let headerToggle = document.querySelector('.main-header__toggle');
 let navContainer = document.querySelector('.main-nav');
+let burgerButton = document.querySelector('.main-header__toggle');
 
 navContainer.classList.remove('main-nav--no-js');
+burgerButton.classList.remove('main-header__toggle--no-js');
 
 headerToggle.addEventListener('click', function () {
   if (mainHeader.classList.contains('main-header--closed')) {
